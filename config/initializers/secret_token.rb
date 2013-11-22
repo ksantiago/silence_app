42b70f0e4548ad958080c9190aab753a1217af5f
Katherine Santiago <santiago.kat@gmail.com>
2013-11-22 09:15:44 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SilenceApp::Application.config.secret_token = '80a94a2331ebb8d4ff4cfd65b624470acfef9c47fd009a89cb29b45b42718ac431658746804eae61cceeb633d9b8b1fe2d1e11d1bb3115743208791f015624d2'
